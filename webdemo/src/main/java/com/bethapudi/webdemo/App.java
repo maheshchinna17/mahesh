14e530a49af62cda792ad2d4756a330ed6ae2563
maheshwar reddy bobati <maheshwar reddy bobati>
2016-08-06 22:09:15 -0700

    New Change

---
+++ webdemo/src/main/java/com/bethapudi/webdemo/App.java
@@ -0,0 +1,17 @@
+package com.bethapudi.webdemo;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(3*2);
+    }
+}
